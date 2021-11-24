5c2741ba4e9c77e11e981cb27d1fa1391aa66334
Guilherme Gules Moreira <guilhermegules@gmail.com>
2021-11-23 21:16:38 -0300

    separating sprites on function

--- raccoon/main.c
+++ raccoon/main.c
@@ -11,11 +11,22 @@
 
 sfIntRect figures[5][5] =
 {
-   {320,  0, RACCOON_WIDTH, RACCOON_HEIGHT},
-   {640,  0, RACCOON_WIDTH, RACCOON_HEIGHT},
-   {960,  0, RACCOON_WIDTH, RACCOON_HEIGHT},
-   {1280, 0, RACCOON_WIDTH, RACCOON_HEIGHT}
+    {320,  0, RACCOON_WIDTH, RACCOON_HEIGHT},
+    {640,  0, RACCOON_WIDTH, RACCOON_HEIGHT},
+    {960,  0, RACCOON_WIDTH, RACCOON_HEIGHT},
+    {1280, 0, RACCOON_WIDTH, RACCOON_HEIGHT}
 };
+
+sfSprite* createSprite(sfIntRect spriteFrame, char* imagePath)
+{
+    sfTexture *texture = sfTexture_createFromFile(imagePath, NULL);
+    sfSprite *draw = sfSprite_create();
+
+    sfSprite_setTexture(draw, texture, 0);
+    sfSprite_setTextureRect(draw, spriteFrame);
+
+    return draw;
+}
 
 int main()
 {
@@ -24,6 +35,9 @@ int main()
     sfVideoMode videoMode = {800, 600, 32};
     window = sfRenderWindow_create(videoMode, "T-Game", sfResize | sfClose, NULL);
     int frameIdx=0;
+    float posX = 0.0f, posY = 0.0f;
+
+    sfSprite *raccoon;
 
     while(sfRenderWindow_isOpen(window))
     {
@@ -47,17 +61,38 @@ int main()
             sfClock_restart(relogio);
         }
 
+        if(sfKeyboard_isKeyPressed(sfKeyRight))
+        {
+            posX += 1.0;
+        }
+
+        if(sfKeyboard_isKeyPressed(sfKeyLeft))
+        {
+            posX -= 1.0;
+        }
+
+        if(sfKeyboard_isKeyPressed(sfKeyUp))
+        {
+            posY -= 1.0;
+        }
+
+        if(sfKeyboard_isKeyPressed(sfKeyDown))
+        {
+            posY += 1.0;
+        }
+
+
         spriteFrame = figures[frameIdx][0];
+        raccoon = createSprite(spriteFrame, "assets/texugo.jpeg");
 
-        sfRenderWindow_clear(window, sfWhite);
+        sfRenderWindow_clear(window, sfBlack);
 
-        sfTexture *textura = sfTexture_createFromFile("assets/texugo.jpeg", NULL);
-        sfSprite *desenho = sfSprite_create();
+        sfSprite_setPosition(raccoon, (sfVector2f)
+        {
+            posX, posY
+        });
 
-        sfSprite_setTexture(desenho, textura, 0);
-        sfSprite_setTextureRect(desenho, spriteFrame);
-
-        sfRenderWindow_drawSprite(window, desenho, NULL);
+        sfRenderWindow_drawSprite(window, raccoon, NULL);
         sfRenderWindow_display(window);
     }
 }
