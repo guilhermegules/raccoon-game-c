bdda044ad3d23c7bf90fde90e4896d7bcfe8a355
SaldanhaVitor <vitorgtfo@gmail.com>
2021-11-30 00:09:56 -0300

    #hearts and dog

--- raccoon/main.c
+++ raccoon/main.c
@@ -11,8 +11,8 @@
 #define RACCOON_HEIGHT 85
 #define RACCON_SPRITES 4
 
-#define DOG_HEIGHT 23.8
-#define DOG_WIDTH 82.3
+#define DOG_HEIGHT 122
+#define DOG_WIDTH 161.2
 #define DOG_SPRITES 5
 
 #define LIVES 3
@@ -27,11 +27,11 @@ sfIntRect racconSprites[RACCON_SPRITES] =
 
 sfIntRect dogSprites[DOG_SPRITES] =
 {
-    {82.3,  0, DOG_WIDTH, DOG_HEIGHT},
-    {164.6,  0, DOG_WIDTH, DOG_HEIGHT},
-    {246.9,  0, DOG_WIDTH, DOG_HEIGHT},
-    {329.2,  0, DOG_WIDTH, DOG_HEIGHT},
-    {411.5,  0, DOG_WIDTH, DOG_HEIGHT}
+    {161.2,  0, DOG_WIDTH, DOG_HEIGHT},
+    {322.4,  0, DOG_WIDTH, DOG_HEIGHT},
+    {483.6,  0, DOG_WIDTH, DOG_HEIGHT},
+    {644.8,  0, DOG_WIDTH, DOG_HEIGHT},
+    {806  ,  0, DOG_WIDTH, DOG_HEIGHT}
 };
 
 sfSprite* createSprite(sfIntRect spriteFrame, char* imagePath)
@@ -53,45 +53,75 @@ int main()
     window = sfRenderWindow_create(videoMode, "Raccoon Game", sfResize | sfClose, NULL);
     int frameIdx = 0;
     float posX = 0.0f, posY = 0.0f;
+    float dposX = 0.0f, dposY = 0.0f;
 
     // Sprites
     sfSprite *raccoon;
     sfVector2f raccoonPos;
 
-    // sfSprite *dog;
+    sfSprite *dog;
+    sfVector2f dogPos;
+    sfVector2f liveTAM;
+    liveTAM.x = 0.15;
+    liveTAM.y = 0.15;
 
-    //sfTexture *liveTexture = sfTexture_createFromFile("assets/heart.png", NULL);
+    //HEART
+    sfTexture *liveTexture = sfTexture_createFromFile("assets/heart.png", NULL);
+    sfSprite *live = sfSprite_create();
 
-    //sfSprite *live = sfSprite_create();
+    sfSprite_setTexture(live, liveTexture, 0);
 
-    //sfSprite_setTexture(live, liveTexture, 0);
+    sfIntRect liveFrame;
+    liveFrame.height = 323;
+    liveFrame.width = 378;
 
-    //sfIntRect liveFrame;
-    //liveFrame.height = 323;
-    //liveFrame.width = 378;
-    //liveFrame.top = 0;
-    //liveFrame.left = 0;
+    sfSprite_scale(live, liveTAM);
+    sfSprite_setTextureRect(live, liveFrame);
 
-    //sfSprite_scale(live, (sfVector2f)
-    //{
-    //    0.15f, 0.15f
-    //});
+    sfSprite_setPosition(live, (sfVector2f)
+    {
+        720.0f, 0.9f
+    });
 
-    //sfSprite_setTextureRect(live, liveFrame);
+    sfSprite *live_2 = sfSprite_copy(live);
+    sfSprite *live_3 = sfSprite_copy(live);
+    sfSprite_setPosition(live_2, (sfVector2f)
+    {
+        650.0f, 0.9f
+    });
+    sfSprite_setPosition(live_3, (sfVector2f)
+    {
+        580.0f, 0.9f
+    });
+    //END ALL HEART
 
-    //sfSprite_setPosition(live, (sfVector2f)
-    //{
-    //    5.00f, 5.00f
-    //});
+
+    // ====================================================== EMPTY HEART ======================================================
+    sfTexture *liveEmptyTexture = sfTexture_createFromFile("assets/emptyHeart.png", NULL);
+    sfSprite *liveEmpty = sfSprite_create();
+
+    sfSprite_setTexture(liveEmpty, liveEmptyTexture, 0);
+
+    sfIntRect liveEmptyFrame;
+    liveEmptyFrame.height = 323;
+    liveEmptyFrame.width = 508;
+
+    sfVector2f liveEmptyTAM;
+    liveEmptyTAM.x = 1.3;
+    liveEmptyTAM.y = 1.3;
+
+    sfSprite_scale(liveEmpty, liveEmptyTAM);
+    sfSprite_setTextureRect(liveEmpty, liveEmptyFrame);
+
+    sfSprite_setPosition(liveEmpty, (sfVector2f)
+    {
+       500.0f, 0.9f
+    });
+    // ====================================================== END EMPTY HEART ======================================================
 
     while(sfRenderWindow_isOpen(window))
     {
         sfEvent event;
-
-        if(raccoon != NULL)
-        {
-            raccoonPos = sfSprite_getPosition(raccoon);
-        }
 
         while(sfRenderWindow_pollEvent(window, &event))
         {
@@ -107,23 +137,23 @@ int main()
         {
             if(raccoonPos.x < 690)
             {
-                posX += 1.0;
+                posX += 3.0;
             }
         }
 
         if(sfKeyboard_isKeyPressed(sfKeyLeft))
         {
-            if(raccoonPos.x != -15)
+           if(raccoonPos.x != -15)
             {
-                posX -= 1.0;
+                posX -= 3.0;
             }
         }
 
         if(sfKeyboard_isKeyPressed(sfKeyUp))
         {
-            if(raccoonPos.y > -20)
+           if(raccoonPos.y > -20)
             {
-                posY -= 1.0;
+                posY -= 3.0;
             }
         }
 
@@ -131,7 +161,7 @@ int main()
         {
             if(raccoonPos.y < 530)
             {
-                posY += 1.0;
+                posY += 3.0;
             }
         }
 
@@ -145,8 +175,7 @@ int main()
         }
 
         raccoon = createSprite(racconSprites[frameIdx], "assets/texugo.png");
-
-        // dog = createSprite(dogSprites[frameIdx], "assets/dog.png");
+        dog     = createSprite(   dogSprites[frameIdx], "assets/dog.png"   );
 
         sfRenderWindow_clear(window, sfWhite);
 
@@ -155,17 +184,34 @@ int main()
             posX, posY
         });
 
+        sfSprite_setPosition(dog, (sfVector2f)
+        {
+            dposX, dposY
+        });
+
+
+        sfRenderWindow_drawSprite(window, dog, NULL);
         sfRenderWindow_drawSprite(window, raccoon, NULL);
+        sfRenderWindow_drawSprite(window, live, NULL);
+        sfRenderWindow_drawSprite(window, live_2, NULL);
+        sfRenderWindow_drawSprite(window, live_3, NULL);
+        sfRenderWindow_drawSprite(window, liveEmpty, NULL);
 
-        //sfRenderWindow_drawSprite(window, dog, NULL);
+        if(raccoon != NULL)
+        {
+            raccoonPos = sfSprite_getPosition(raccoon);
+        }
 
-        //sfRenderWindow_drawSprite(window, live, NULL);
+        if(dog!= NULL)
+        {
+            dogPos = sfSprite_getPosition(dog);
+        }
 
         sfRenderWindow_display(window);
     }
 
     sfSprite_destroy(raccoon);
-    //sfSprite_destroy(live);
-    //sfSprite_destroy(dog);
+    sfSprite_destroy(live);
+    sfSprite_destroy(dog);
     sfRenderWindow_destroy(window);
 }
